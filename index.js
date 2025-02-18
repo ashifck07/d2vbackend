4116ffc0b56b3275b7359a7f619250f985683120
ashifck07 <ashifck.stackup@gmail.com>
2025-02-18 15:09:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const {checkFirebaseConnection} = require("./config/firebase");
+
+const architectRoutes = require("./routes/architectRoutes");
+const leadRoutes = require("./routes/leadRoutes");
+const designRoutes = require("./routes/designRoutes");
+const activityLogRoutes = require("./routes/activitylogRoute");
+const userRoutes = require("./routes/userRoutes");
+const customerRoutes = require("./routes/customerRoute");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json()); // Parses incoming JSON requests
+app.use(express.urlencoded({extended: true})); // Parses URL-encoded requests (optional)
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    // origin: ["http://localhost:3000", "http://your-production-domain.com"] ----- enable during production
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
+app.use("/architect", architectRoutes);
+app.use("/leads", leadRoutes);
+app.use("/designs", designRoutes);
+app.use("/activityLog", activityLogRoutes);
+app.use("/user", userRoutes);
+app.use("/customer", customerRoutes);
+app.use("/files", require("./routes/imgRoute"));
+app.use("/workProgress", require("./routes/workProgressRoute"));
+
+const port = process.env.PORT || 5000;
+
+checkFirebaseConnection()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error initializing the app:", error.message);
+  });
